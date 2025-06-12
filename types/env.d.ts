f602de17346934c09c1f08b64aee8f872784fe12
Mohammed hisham <96807846+Hisham1404@users.noreply.github.com>
2025-06-12 22:01:09 +0530

    Setup ElevenLabs Conversational Agent

--- types/env.d.ts
+++ types/env.d.ts
@@ -4,6 +4,7 @@ declare global {
       EXPO_PUBLIC_SUPABASE_URL: string;
       EXPO_PUBLIC_SUPABASE_ANON_KEY: string;
       EXPO_PUBLIC_ELEVENLABS_API_KEY: string;
+      EXPO_PUBLIC_ELEVENLABS_AGENT_ID: string;
     }
   }
 }
